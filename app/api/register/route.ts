5268de451326f2dbd1bcb340f8146ea600a6ca29
douhailong <1804610117@11.com>
2023-09-15 18:05:43 +0800

    add page

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,24 +1,22 @@
-import bcrypt from 'bcrypt';
 import { NextResponse } from 'next/server';
+import bcrypt from 'bcrypt';
 
 import prisma from '@/app/libs/prisma';
 
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
-    const { email, name, password } = body;
+    const { email, name, password } = await request.json();
 
     if (!email || !name || !password) {
-      return new Response('Missing info', { status: 400 });
+      return new NextResponse('Missing info', { status: 400 });
     }
 
     const hashedPassword = await bcrypt.hash(password, 12);
-
     const user = await prisma.user.create({ data: { email, name, hashedPassword } });
 
     return NextResponse.json(user);
   } catch (err) {
     console.log('REGISTER_ERROR:', err);
-    return new Response('Internal error', { status: 500 });
+    return new NextResponse('Internal error', { status: 500 });
   }
 }
