fd1e8bb9be264f68a242248751dacf79da533f4c
douhailong <1804610117@11.com>
2023-10-07 17:43:24 +0800

    item

--- app/users/components/user-item.tsx
+++ app/users/components/user-item.tsx
@@ -1,11 +1,25 @@
 import type { User } from '@prisma/client';
 
+import Avatar from '@/app/components/avatar';
+
 interface UserItemProps {
   user?: User;
 }
 
 const UserItem: React.FC<UserItemProps> = ({ user }) => {
-  return <div>UserItem UserItem</div>;
+  return (
+    <div className='flex cursor-pointer items-center space-x-3 rounded-lg bg-white p-3 hover:bg-neutral-100'>
+      <Avatar active />
+      <div className='min-w-0 flex-1'>
+        <div className='focus:outline-none'>
+          <span className='absolute inset-0' aria-hidden='true' />
+          <div className='mb-1 flex items-center justify-between'>
+            <p className='text-sm font-medium text-gray-900'>{'data.name'}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserItem;
--- app/users/components/user-list.tsx
+++ app/users/components/user-list.tsx
@@ -1,8 +1,8 @@
-import UserItem from "./user-item";
+import UserItem from './user-item';
 
 const UserList = () => {
   return (
-    <aside className='fixed inset-y-0 left-0 w-full pb-20 lg:left-20 lg:w-80 lg:pb-0'>
+    <aside className='overfloe-y-auto fixed inset-y-0 left-0 w-full border-r border-gray-200 pb-20 lg:left-20 lg:w-80 lg:pb-0'>
       <div className='px-5'>
         <div className='py-4 text-2xl font-bold text-neutral-800'>People</div>
         {[1, 2, 3, 4, 5, 6].map((i) => (
